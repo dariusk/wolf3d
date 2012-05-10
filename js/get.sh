0c2b9dafadae5e622840a464a509bbe6037acd0a
Darius Kazemi <darius.kazemi@gmail.com>
2012-05-10 15:34:31 -0400

    wolf

---
+++ js/get.sh
@@ -0,0 +1,56 @@
+wget http://wolfenstein.bethsoft.com/game/js/requestAnimFrame.js
+
+wget http://wolfenstein.bethsoft.com/game/js/wolf.js
+
+wget http://wolfenstein.bethsoft.com/game/js/random.js
+
+wget http://wolfenstein.bethsoft.com/game/js/angle.js
+
+wget http://wolfenstein.bethsoft.com/game/js/math.js
+
+wget http://wolfenstein.bethsoft.com/game/js/input.js
+
+wget http://wolfenstein.bethsoft.com/game/js/sound.js
+
+wget http://wolfenstein.bethsoft.com/game/js/menu.js
+
+wget http://wolfenstein.bethsoft.com/game/js/file.js
+
+wget http://wolfenstein.bethsoft.com/game/js/episodes.js
+
+wget http://wolfenstein.bethsoft.com/game/js/maps.js
+
+wget http://wolfenstein.bethsoft.com/game/js/game.js
+
+wget http://wolfenstein.bethsoft.com/game/js/player.js
+
+wget http://wolfenstein.bethsoft.com/game/js/sprites.js
+
+wget http://wolfenstein.bethsoft.com/game/js/powerups.js
+
+wget http://wolfenstein.bethsoft.com/game/js/ai.js
+
+wget http://wolfenstein.bethsoft.com/game/js/actorai.js
+
+wget http://wolfenstein.bethsoft.com/game/js/actors.js
+
+wget http://wolfenstein.bethsoft.com/game/js/actstat.js
+
+wget http://wolfenstein.bethsoft.com/game/js/weapon.js
+
+wget http://wolfenstein.bethsoft.com/game/js/doors.js
+
+wget http://wolfenstein.bethsoft.com/game/js/pushwall.js
+
+wget http://wolfenstein.bethsoft.com/game/js/areas.js
+
+wget http://wolfenstein.bethsoft.com/game/js/level.js
+
+wget http://wolfenstein.bethsoft.com/game/js/raycaster.js
+wget http://wolfenstein.bethsoft.com/game/js/renderer.js
+
+wget http://wolfenstein.bethsoft.com/game/js/modernizr.custom.js
+
+wget http://wolfenstein.bethsoft.com/game/js/jquery-1.7.1.min.js
+
+wget http://wolfenstein.bethsoft.com/game/js/load.js
